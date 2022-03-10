f53757010db2f2523de28a64cd5c7c79298bd086
Přemysl Fára <premysl.fara@ezpada.com>
2022-03-10 07:52:07 +0100

    Updated results.

---
+++ DocumentsStore.Core.Tests/Result_Tests.cs
@@ -0,0 +1,138 @@
+﻿/* DocumentsStore - (C) 2022 Premysl Fara  */
+
+namespace DocumentsStore.Core.Tests;
+
+using System;
+    
+using Xunit;
+
+
+public class Result_Tests
+{
+    [Fact]
+    public void SimpleResult_Ok_IsSuccess()
+    {
+        Assert.True(SimpleResult.Ok().IsSuccess);
+    }
+    
+    [Fact]
+    public void SimpleResult_Ok_MessageIsOk()
+    {
+        Assert.Equal("Ok", SimpleResult.Ok().Message);
+    }
+    
+    [Fact]
+    public void SimpleResult_Ok_DataTypeIsObject()
+    {
+        Assert.Equal(typeof(object).FullName, SimpleResult.Ok().GetType().BaseType?.FullName);
+    }
+    
+    [Fact]
+    public void SimpleResult_Ok_MessageIsSet()
+    {
+        var msg = "Test message";
+        
+        Assert.Equal(msg, SimpleResult.Ok(msg).Message);
+    }
+    
+    [Fact]
+    public void SimpleResult_Ok_DataIsSet()
+    {
+        var data = new object();
+        
+        Assert.Equal(data, ((IResult<object>)SimpleResult.Ok(data)).Data);
+    }
+    
+    
+    [Fact]
+    public void SimpleResult_Error_IsNotSuccess()
+    {
+        Assert.False(SimpleResult.Error().IsSuccess);
+    }
+    
+    [Fact]
+    public void SimpleResult_Error_MessageIsError()
+    {
+        Assert.Equal("Error", SimpleResult.Error().Message);
+    }
+    
+    [Fact]
+    public void SimpleResultError_MessageIsSet()
+    {
+        var msg = "Test message";
+        
+        Assert.Equal(msg, SimpleResult.Error(msg).Message);
+    }
+    
+    [Fact]
+    public void SimpleResult_Error_DataTypeIsObject()
+    {
+        Assert.Equal(typeof(object).FullName, SimpleResult.Error().GetType().BaseType?.FullName);
+    }
+    
+    [Fact]
+    public void SimpleResult_Error_MessageIsSet()
+    {
+        var msg = "Test message";
+        
+        Assert.Equal(msg, SimpleResult.Error(msg).Message);
+    }
+    
+    [Fact]
+    public void SimpleResult_Error_DataIsSet()
+    {
+        var data = new object();
+        
+        Assert.Equal(data, ((IResult<object>)SimpleResult.Error(data)).Data);
+    }
+    
+    
+    [Fact]
+    public void SimpleResult_FromTrue_IsSuccess()
+    {
+        Assert.True(SimpleResult.FromBoolean(true).IsSuccess);
+    }
+    
+    [Fact]
+    public void SimpleResult_FromTrue_IsOk()
+    {
+        Assert.Equal("Ok", SimpleResult.FromBoolean(true).Message);
+    }
+    
+    [Fact]
+    public void SimpleResult_FromFalse_IsNotSuccess()
+    {
+        Assert.False(SimpleResult.FromBoolean(false).IsSuccess);
+    }
+    
+    [Fact]
+    public void SimpleResult_FromFalse_IsError()
+    {
+        Assert.Equal("Error", SimpleResult.FromBoolean(false).Message);
+    }
+    
+    [Fact]
+    public void Result_FromException_ContainsException()
+    {
+        var exception = new Exception("Something is wrong!");
+        
+        Assert.Equal(exception, Result<Exception>.Error(exception).Data);
+    }
+    
+    [Fact]
+    public void Result_FromException_ContainsMessageFromExceptionIfNoMessageIsSet()
+    {
+        var exception = new Exception("Something is wrong!");
+        
+        Assert.Equal(exception.Message, Result<Exception>.Error(exception).Message);
+    }
+    
+    [Fact]
+    public void Result_FromException_ContainsMessageIfMessageIsSet()
+    {
+        var message = "Something ELSE if wrong...";
+        var exception = new Exception("Something is wrong!");
+        
+        Assert.Equal(message, Result<Exception>.Error(exception, message).Message);
+    }
+}
--- DocumentsStore.Core/Result.cs
+++ DocumentsStore.Core/Result.cs
@@ -2,41 +2,81 @@
 
 namespace DocumentsStore.Core;
 
+using System;
+    
+    
+/// <summary>
+/// The default IResult implementation.
+/// </summary>
+/// <typeparam name="T">A type of the data related to an operation result.</typeparam>
 public class Result<T> : IResult<T>
 {
+    private const string OkMessage = "Ok";
+    private const string ErrorMessage = "Error";
+    
     public bool IsSuccess { get; }
     public string Message { get; }
     public T? Data { get; }
 
-
-    private Result(bool isSuccess, T? data, string message)
+   
+    private Result(bool isSuccess, T? data = default, string message = "")
     {
         IsSuccess = isSuccess;
         Data = data;
         Message = message;
     }
+    
 
-
-    public static IResult<T> Ok(string? message = null)
+    /// <summary>
+    /// Creates a successful operation result. 
+    /// </summary>
+    /// <param name="message">An optional message. "Ok" by default.</param>
+    /// <returns>An IResult&lt;T&gt; instance.</returns>
+    public static IResult<T> Ok(string? message = default)
     {
-        return new Result<T>(true, default, message ?? "Ok");
+        return new Result<T>(true, default, message ?? OkMessage);
     }
 
-
-    public static IResult<T> Ok(T? data, string? message = null)
+    /// <summary>
+    /// Creates a successful operation result. 
+    /// </summary>
+    /// <param name="data">Data related to the option result.</param>
+    /// <param name="message">An optional message. "Ok" by default.</param>
+    /// <returns>An IResult&lt;T&gt; instance.</returns>
+    public static IResult<T> Ok(T? data, string? message = default)
     {
-        return new Result<T>(true, data ?? default, message ?? "Ok");
+        return new Result<T>(true, data, message ?? OkMessage);
+    }
+    
+    /// <summary>
+    /// Creates a failed operation result. 
+    /// </summary>
+    /// <param name="message">An optional message. "Error" by default.</param>
+    /// <returns>An IResult&lt;T&gt; instance.</returns>
+    public static IResult<T> Error(string? message = default)
+    {
+        return new Result<T>(false, default, message ?? ErrorMessage);
     }
 
-
-    public static IResult<T> Error(string? message = null)
+    /// <summary>
+    /// Creates a failed operation result. 
+    /// </summary>
+    /// <param name="data">Data related to the option result.</param>
+    /// <param name="message">An optional message. "Error" by default.</param>
+    /// <returns>An IResult&lt;T&gt; instance.</returns>
+    public static IResult<T> Error(T? data, string? message = default)
     {
-        return new Result<T>(false, default, message ?? "Ok");
+        return new Result<T>(false, data, message ?? ErrorMessage);
     }
-
-
-    public static IResult<T> Error(T? data, string? message = null)
+    
+    /// <summary>
+    /// Creates a failed operation result based on an exception. 
+    /// </summary>
+    /// <param name="ex">An exception related to the option result.</param>
+    /// <param name="message">An optional message. Exception.Message by default.</param>
+    /// <returns>An IResult&lt;Exception&gt; instance.</returns>
+    public static IResult<Exception> Error(Exception ex, string? message = default)
     {
-        return new Result<T>(false, data ?? default, message ?? "Ok");
+        return new Result<Exception>(false, ex, message ?? ex.Message);
     }
 }
